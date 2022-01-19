f715f16022e310444c978d0007d799f1c9cadcf8
jackmontoro <jmontoro@ucsd.edu>
2022-01-19 15:40:29 -0800

    Corrected the error in test-file- nonlink parens

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,10 +12,13 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            int nextCloseBracket = markdown.indexOf("](", nextOpenBracket);
+            int closeParen = markdown.indexOf(")", nextCloseBracket + 1);
+            if(nextCloseBracket != -1 && nextCloseBracket != -1){
+                toReturn.add(markdown.substring(nextCloseBracket + 2, closeParen));
+            } else {
+                break;
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
