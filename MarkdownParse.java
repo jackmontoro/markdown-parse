ceddc96752877737835147f967717e0489ccbb59
jackmontoro <jmontoro@ucsd.edu>
2022-01-28 14:38:35 -0800

    brackets error fixed

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,7 +15,11 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (openParen != -1 && closeParen != -1){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            } else {
+                break;
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
